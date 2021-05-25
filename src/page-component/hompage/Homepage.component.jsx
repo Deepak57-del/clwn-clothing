c7e84ce494e8e5338ab2ced371270cf0e07bc43d
Deepak57-del <deepak1247@chitkara.edu.in>
2021-05-25 12:06:31 +0530

    broken hompage in smaller components is Finished and styling of that component is also done

---
+++ src/page-component/hompage/Homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Directory from '../../components/direcotry/directory.component';
+import '../hompage/Homepage.style.css'
+const HomePage = ()=>(
+    <div className="homepage">
+        <Directory/>
+    </div>
+)
+export default HomePage;
